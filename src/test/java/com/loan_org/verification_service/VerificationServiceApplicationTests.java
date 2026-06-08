dfe3cae0654707fb3b8087d7ff511e676d279a50
Aman Raj <billygates124356@gmail.com>
2026-06-08 15:05:49 +0530

    Initial commit

---
+++ src/test/java/com/loan_org/verification_service/VerificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loan_org.verification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
