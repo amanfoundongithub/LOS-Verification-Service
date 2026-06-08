4ec68ef3bf376cf14e9860d30f8bb7ac3f01de10
Aman Raj <billygates124356@gmail.com>
2026-06-08 22:34:37 +0530

    added contract for document processor

---
+++ src/main/java/com/loan_org/verification_service/core/processor/DocumentProcessor.java
@@ -0,0 +1,10 @@
+package com.loan_org.verification_service.core.processor;
+
+import com.loan_org.verification_service.api.dto.ProcessResponse;
+
+import java.awt.image.BufferedImage;
+
+public interface DocumentProcessor {
+    ProcessResponse process(BufferedImage image);
+    String getSupportedDocumentType();
+}
