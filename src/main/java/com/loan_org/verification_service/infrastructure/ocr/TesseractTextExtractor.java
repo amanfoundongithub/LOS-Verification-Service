518eab0dd9d8f66e5bceb44de69d14729fd6dc2b
Aman Raj <billygates124356@gmail.com>
2026-06-08 21:15:33 +0530

    concrete implementation using tesseract

---
+++ src/main/java/com/loan_org/verification_service/infrastructure/ocr/TesseractTextExtractor.java
@@ -0,0 +1,41 @@
+package com.loan_org.verification_service.infrastructure.ocr;
+
+import com.loan_org.verification_service.api.exception.VerificationErrorCode;
+import com.loan_org.verification_service.api.exception.VerificationException;
+import com.loan_org.verification_service.core.ocr.TextExtractor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+import org.springframework.stereotype.Component;
+
+import java.awt.image.BufferedImage;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class TesseractTextExtractor implements TextExtractor {
+
+    private final Tesseract tesseract;
+
+    @Override
+    public String extract(BufferedImage image) {
+
+        log.info("Performing OCR extraction on image...");
+        if(image == null) {
+            throw new VerificationException(
+                    "Cannot perform OCR processing on a null image",
+                    VerificationErrorCode.EMPTY_PAYLOAD);
+        }
+
+        try {
+            return tesseract.doOCR(image);
+        } catch (TesseractException e) {
+            throw new VerificationException(
+                    "Tesseract failure during OCR:" + e.getMessage(), e,
+                    VerificationErrorCode.OCR_PARSING_ERROR);
+        }
+
+    }
+
+}
