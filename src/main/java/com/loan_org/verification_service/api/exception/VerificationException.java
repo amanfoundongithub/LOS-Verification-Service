3f1871b23e4b4aca695135545e0f37a6bb98220d
Aman Raj <billygates124356@gmail.com>
2026-06-08 20:15:56 +0530

    updated exception with fallback and string manipulation

--- src/main/java/com/loan_org/verification_service/api/exception/VerificationException.java
+++ src/main/java/com/loan_org/verification_service/api/exception/VerificationException.java
@@ -1,17 +1,41 @@
 package com.loan_org.verification_service.api.exception;
 
+import lombok.Getter;
+
+@Getter
 public class VerificationException extends RuntimeException {
 
-    public VerificationException(String message) {
+    private final VerificationErrorCode errorCode;
+
+    public VerificationException(String message, VerificationErrorCode errorCode) {
         super(message);
+        this.errorCode = errorCode;
     }
 
-    public VerificationException(String message, Throwable cause) {
+    public VerificationException(String message, Throwable cause, VerificationErrorCode errorCode) {
         super(message, cause);
+        this.errorCode = errorCode;
     }
 
-    public VerificationException(Throwable cause) {
+    public VerificationException(Throwable cause, VerificationErrorCode errorCode) {
         super(cause);
+        this.errorCode = errorCode;
+    }
+
+    public VerificationException(VerificationErrorCode errorCode) {
+        super("Document verification failed with error code: " + errorCode);
+        this.errorCode = errorCode;
+    }
+
+    @Override
+    public String toString() {
+
+        return String.format("%s [%s]: %s",
+                getClass().getName(),
+                this.errorCode,
+                getLocalizedMessage()
+        );
+
     }
 
 }
